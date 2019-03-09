f400f0cbd667101a56a8692bee9110bf6d683029
Jonathan <jonathan@jonathans-imac.lan>
2019-03-08 23:03:47 -0500

    Massive UI updates

--- src/components/NetworkList.js
+++ src/components/NetworkList.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import {StyleSheet, Text, View, ScrollView} from 'react-native';
-import Accordion from 'react-native-collapsible/Accordion'
+import {Button, Icon} from 'react-native-elements'
+import Accordion from 'react-native-collapsible/Accordion';
 
-const SECTIONS = [
-    {
-      title: 'First',
-      content: 'Lorem ipsum...'
-    },
-    {
-      title: 'Second',
-      content: 'Lorem ipsum...'
-    }
-  ];
 
 class NetworkList extends React.Component {
+
   constructor(props){
       super(props)
       this.state = {
@@ -21,21 +13,31 @@ class NetworkList extends React.Component {
       }
   }
  
-  _renderHeader = section => {
+  _renderHeader = (section, index) => {
+    console.log(index)
     return (
-      <View>
-        <Text style={styles.header}>Device 1</Text>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Device {index + 1}</Text>
       </View>
     );
   };
  
   _renderContent = section => {
+  
     return (
-      <View>
-        <Text style={styles.item}>{section.mac_address}</Text>
-        <Text style={styles.item}>{section.ip_address}</Text>
-        <Text style={styles.item}>{section.ul_data}</Text>
-        <Text style={styles.item}>{section.dl_data}</Text>
+      <View style={styles.items}>
+        <Text style={styles.mac_address}>Mac Adress: {section.mac_address}</Text>
+        <Text style={styles.mac_address}>IP Adress:{section.ip_address}</Text>
+        <View style={styles.footer}>
+          <Text style={styles.mac_address}>UpLink: {section.ul_data}  </Text>
+          <Text style={styles.mac_address}>Downlink: {section.dl_data}</Text>
+        </View>
+        <Button icon={{
+          name: 'speedometer',
+          size: 35,
+          color: '#ff0000',
+          type: "material-community"
+        }} buttonStyle={{borderColor: '#e84a4a', borderWidth: 1.5}} title="Class Type: 10" type="outline" titleStyle={{fontSize: 23, color: '#ff0000'}}></Button>
       </View>
     );
   };
@@ -48,7 +50,7 @@ class NetworkList extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.List}>Device List</Text>
+        <Text style={styles.title}>Device List</Text>
         <ScrollView contentInset={{top: 0, left: 0, bottom: 0, right:0}} bounces={false}>
             <Accordion
                 sections={this.props.networkDevices}
@@ -56,6 +58,7 @@ class NetworkList extends React.Component {
                 renderHeader={this._renderHeader}
                 renderContent={this._renderContent}
                 onChange={this._updateSections}
+                sectionContainerStyle={{marginBottom: 10}}
             />
         </ScrollView>
       </View>
@@ -65,23 +68,38 @@ class NetworkList extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
-        marginTop: 80,
+        flex: 1,
+        marginTop: 30,
         width: '100%',
-        
     },
-    List: {
-      fontSize: 30,
+    title: {
+      fontSize: 50,
       textAlign: 'center'
     },
-    item: {
+    items: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      textAlign: 'justify'
+
+    },
+    mac_address: {
       fontSize: 20,
-      textAlign: 'left',
-      paddingLeft: 60
+      marginBottom: 15
     },
     header : {
-        fontSize: 50,
-        paddingLeft: 60
-    }
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginBottom: 10
+    },
+    headerText: {
+      fontSize: 40,
+    },
+    footer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
 })
 
 export default NetworkList;
