8782f1e5ff476bfb6a8bc491dad2b883a4a6db4b
Jonathan <jonathan@jonathans-imac.lan>
2019-03-12 03:07:46 -0400

    update

--- src/screens/DeviceSettings.js
+++ src/screens/DeviceSettings.js
@@ -1,16 +1,35 @@
 import React from 'react';
-import {StyleSheet, Text, View, Dimensions, Modal, TouchableOpacity} from 'react-native';
+import {StyleSheet, Text, View, Dimensions, Modal, TouchableOpacity, FlatList, TouchableHighlight} from 'react-native';
 import {Overlay, CheckBox, Input, Icon} from 'react-native-elements'
 
-const networkAllocations = {
-    className10: '10',
-    className25: '25',
-    className50: '50',
-    className100: '100',
-    className250: '250',
-    className500: '500',
-    className1000: '1000',
-  }
+const networkAllocations = [{
+    className: '10', id: '1'},
+    {   
+        id: '2',
+        className: '25'
+    },
+
+    {   
+        id: '3',
+        className: '50'
+    },
+    {   
+        id: '4',
+        className: '100'
+    },
+    {   
+        id: '5',
+        className: '250'
+    },
+    {   
+        id: '6',
+        className: '500'
+    },
+    {   
+        id: '7',
+        className: '1000'
+    },
+]
 
 const screen_Height = Dimensions.get('window').height;
 const screen_Width = Dimensions.get('window').width;
@@ -19,9 +38,22 @@ class DeviceSettings extends React.Component {
 
 constructor(props){
     super(props)
-    this.state= {
+    this.state = {
+        modalVisible: false,
+        networkClass: 'Select From Dropdown'
     }
 }
+
+ setModalVisible(visible){
+     this.setState({modalVisible: visible})
+ }
+
+ setNetworkClass(className, visible){
+    this.setState({
+        networkClass: className
+    })
+    this.setModalVisible(visible)
+ }
 
   render() {
     const deviceName = this.props.navigation.getParam('deviceName', 'No Name') //this gets the param that was set in the networkList screen
@@ -56,15 +88,27 @@ constructor(props){
             </View>
             <View style={styles.dropDown}>
                 <Text style={{marginBottom: 5, paddingLeft: 10, fontSize: 20, color: '#ff0000', fontWeight:'bold'}}>Network Class</Text>
-                <TouchableOpacity style={styles.touchableHeader} onPress={() => {console.log('Testing')}}>
+                <TouchableOpacity style={styles.touchableHeader} onPress={() => {this.setModalVisible(true)}}>
                     <Icon name="speedometer" type="material-community" size={35} iconStyle={{paddingRight: 20}} color='red'></Icon>
-                    <Text style={{fontSize: 20}}>Select From Dropdown</Text>
+                    <Text style={{fontSize: 20}}>{this.state.networkClass}</Text>
                 </TouchableOpacity>
             </View>
-            <Modal transparent={true} visible={true} animationType='fade'>
+            <Modal transparent={true} visible={this.state.modalVisible} animationType='fade'>
                 <View style={styles.modalStyle}>
                     <View style={styles.containerModal}>
-                        <Text>Testing</Text>
+                        <FlatList
+                            data={networkAllocations}
+                            showsVerticalScrollIndicator={true}
+                            renderItem={({item}) =>
+                                <View style={styles.networkClass}>
+                                    <TouchableHighlight underlayColor='#d6d6d6' onLongPress={() => {this.setNetworkClass(item.className, !this.state.modalVisible)}}>
+                                        <Text style={{color: 'black', fontSize: 25}}>{item.className}</Text>
+                                    </TouchableHighlight>
+                                </View>
+                            }
+                            keyExtractor={item => item.id}
+                            >
+                        </FlatList>
                     </View>
                 </View>
             </Modal>
@@ -118,6 +162,9 @@ const styles= StyleSheet.create({
         backgroundColor: 'white',
         paddingLeft: 20,
         paddingTop: 10
+    },
+    networkClass: {
+        marginBottom: 10,
     }
 })
 
